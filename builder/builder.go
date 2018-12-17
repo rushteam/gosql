0c5be646b74d2aad268b0df8314074e5a96f3611
mlboy <mlboy@126.com>
2018-12-17 21:38:16 +0800

    commit

---
+++ builder/builder.go
@@ -0,0 +1,820 @@
+package builder
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+var tagKey = "db"
+var identKey = "`"
+
+//SQLSegments ...
+type SQLSegments struct {
+	table   []Table
+	fields  []string
+	flags   []string
+	join    []map[string]string
+	where   Clause
+	groupBy []string
+	having  Clause
+	orderBy []string
+	limit   struct {
+		limit  int
+		offset int
+	}
+	union     []func(*SQLSegments)
+	forUpdate bool
+	returning bool
+	params    []interface{}
+	render    struct {
+		args []interface{}
+	}
+}
+
+//Table ...
+type Table struct {
+	Name  string
+	Alias string
+}
+
+//NewSQLSegment ...
+func NewSQLSegment() *SQLSegments {
+	return &SQLSegments{}
+}
+
+//Table SQLSegments
+func (s *SQLSegments) Table(name interface{}) *SQLSegments {
+	switch v := name.(type) {
+	case Table:
+		s.table = append(s.table, v)
+	case []Table:
+		s.table = append(s.table, v...)
+	case string:
+		var t = &Table{v, ""}
+		s.table = append(s.table, *t)
+	}
+	return s
+}
+
+//Field SQLSegments
+func (s *SQLSegments) Field(fields ...string) *SQLSegments {
+	if len(fields) > 0 {
+		s.fields = append(s.fields, fields...)
+	}
+	return s
+}
+
+//Flag SQLSegments
+func (s *SQLSegments) Flag(flags ...string) *SQLSegments {
+	if len(flags) > 0 {
+		s.flags = append(s.flags, flags...)
+	}
+	return s
+}
+
+//Join SQLSegments
+func (s *SQLSegments) Join(table string, conditionA, logic, conditionB string) *SQLSegments {
+	s.addJoin("", table, conditionA, logic, conditionB)
+	return s
+}
+
+//LeftJoin SQLSegments
+func (s *SQLSegments) LeftJoin(table string, conditionA, logic, conditionB string) *SQLSegments {
+	s.addJoin("LEFT", table, conditionA, logic, conditionB)
+	return s
+}
+
+//RightJoin SQLSegments
+func (s *SQLSegments) RightJoin(table string, conditionA, logic, conditionB string) *SQLSegments {
+	s.addJoin("RIGHT", table, conditionA, logic, conditionB)
+	return s
+}
+
+//InnerJoin SQLSegments
+func (s *SQLSegments) InnerJoin(table string, conditionA, logic, conditionB string) *SQLSegments {
+	s.addJoin("INNER", table, conditionA, logic, conditionB)
+	return s
+}
+
+//CorssJoin SQLSegments
+func (s *SQLSegments) CorssJoin(table string, conditionA, logic, conditionB string) *SQLSegments {
+	s.addJoin("CROSS", table, conditionA, logic, conditionB)
+	return s
+}
+
+//addJoin SQLSegments
+func (s *SQLSegments) addJoin(typ string, table string, conditionA, logic, conditionB string) *SQLSegments {
+	var t = make(map[string]string)
+	t["type"] = typ
+	t["table"] = table
+	t["logic"] = logic
+	t["conditionA"] = conditionA
+	t["conditionB"] = conditionB
+	s.join = append(s.join, t)
+	return s
+}
+
+//OrderBy SQLSegments
+func (s *SQLSegments) OrderBy(fields ...string) *SQLSegments {
+	if len(fields) > 0 {
+		s.orderBy = append(s.orderBy, fields...)
+	}
+	return s
+}
+
+//GroupBy SQLSegments
+func (s *SQLSegments) GroupBy(fields ...string) *SQLSegments {
+	if len(fields) > 0 {
+		s.groupBy = append(s.groupBy, fields...)
+	}
+	return s
+}
+
+//Offset SQLSegments
+func (s *SQLSegments) Offset(n int) *SQLSegments {
+	s.limit.offset = n
+	return s
+}
+
+//Limit SQLSegments
+func (s *SQLSegments) Limit(n int) *SQLSegments {
+	s.limit.limit = n
+	return s
+}
+
+//ForUpdate SQLSegments
+func (s *SQLSegments) ForUpdate() *SQLSegments {
+	s.forUpdate = true
+	return s
+}
+
+//todo Union SQLSegments
+
+//Clause ...
+type Clause struct {
+	key    interface{}
+	val    interface{}
+	logic  string
+	clause []*Clause
+}
+
+func (p *Clause) addClause(logic string, key interface{}, vals ...interface{}) *Clause {
+	var c = &Clause{}
+	c.logic = logic
+	switch k := key.(type) {
+	case func(*Clause):
+		k(c)
+		// p.clause = append(p.clause, c)
+	default:
+		c.key = key
+		if len(vals) > 0 {
+			c.val = vals[0]
+		}
+	}
+	// fmt.Println(p.clause)
+	p.clause = append(p.clause, c)
+	return p
+}
+
+//Where ..
+func (p *Clause) Where(key interface{}, vals ...interface{}) *Clause {
+	p.addClause("AND", key, vals...)
+	return p
+}
+
+//OrWhere ..
+func (p *Clause) OrWhere(key interface{}, vals ...interface{}) *Clause {
+	p.addClause("OR", key, vals...)
+	return p
+}
+
+//Build ...
+func (p *Clause) Build(i int) (string, []interface{}) {
+	var sql = ""
+	var args []interface{}
+	if p.logic != "" && i > 0 {
+		sql += " " + p.logic
+	}
+	switch k := p.key.(type) {
+	case string:
+		r, _ := regexp.Compile(`\[(\>\=|\<\=|\>|\<|\<\>|\!\=|\=|\~|\!\~|like|!like|in|!in|is|!is|exists|!exists|#)\]?([a-zA-Z0-9_.\-\=\s\?\(\)]*)`)
+		match := r.FindStringSubmatch(k)
+		var context string
+		if len(match) > 0 {
+			// fmt.Println(len(match), match[1])
+			switch match[1] {
+			case "~", "like":
+				context = buildIdent(match[2]) + " LIKE ?"
+				args = append(args, p.val)
+			case "!~", "!like":
+				context = buildIdent(match[2]) + "` NOT LIKE ?"
+				args = append(args, p.val)
+			case ">":
+				context = buildIdent(match[2]) + " > ?"
+				args = append(args, p.val)
+			case ">=":
+				context = buildIdent(match[2]) + "` >= ?"
+				args = append(args, p.val)
+			case "<":
+				context = buildIdent(match[2]) + " < ?"
+				args = append(args, p.val)
+			case "<=":
+				context = buildIdent(match[2]) + " <= ?"
+				args = append(args, p.val)
+			case "<>", "!=":
+				context = buildIdent(match[2]) + " != ?"
+				args = append(args, p.val)
+			case "=":
+				context = buildIdent(match[2]) + " = ?"
+				args = append(args, p.val)
+			case "in":
+				context = buildIdent(match[2]) + " IN (" + buildPlaceholder(p.val) + ")"
+				if reflect.TypeOf(p.val).Kind() == reflect.Slice {
+					v := reflect.ValueOf(p.val)
+					for n := 0; n < v.Len(); n++ {
+						args = append(args, v.Index(n).Interface())
+					}
+				} else {
+					args = append(args, p.val)
+				}
+			case "!in":
+				context = buildIdent(match[2]) + " NOT IN (" + buildPlaceholder(p.val) + ")"
+				if reflect.TypeOf(p.val).Kind() == reflect.Slice {
+					v := reflect.ValueOf(p.val)
+					for n := 0; n < v.Len(); n++ {
+						args = append(args, v.Index(n).Interface())
+					}
+				} else {
+					args = append(args, p.val)
+				}
+			case "exists":
+				switch p.val.(type) {
+				case string:
+					context = "EXISTS (" + p.val.(string) + ")"
+				case func(s *SQLSegments):
+					s := NewSQLSegment()
+					p.val.(func(s *SQLSegments))(s)
+					context = "EXISTS (" + s.BuildSelect() + ")"
+					args = append(args, s.render.args...)
+				}
+			case "!exists":
+				switch p.val.(type) {
+				case string:
+					context = "NOT EXISTS (" + p.val.(string) + ")"
+				case func(s *SQLSegments):
+					s := NewSQLSegment()
+					p.val.(func(s *SQLSegments))(s)
+					context = "NOT EXISTS (" + s.BuildSelect() + ")"
+					args = append(args, s.render.args...)
+				}
+			case "is":
+				if p.val == nil {
+					context = buildIdent(match[2]) + " IS NULL"
+				} else {
+					context = buildIdent(match[2]) + " IS ?"
+					args = append(args, p.val)
+				}
+			case "!is":
+				if p.val == nil {
+					context = buildIdent(match[2]) + " IS NOT NULL"
+				} else {
+					context = buildIdent(match[2]) + " IS NOT ?"
+					args = append(args, p.val)
+				}
+			case "#":
+				context = match[2]
+				if reflect.TypeOf(p.val).Kind() == reflect.Slice {
+					v := reflect.ValueOf(p.val)
+					for n := 0; n < v.Len(); n++ {
+						args = append(args, v.Index(n).Interface())
+					}
+				} else {
+					args = append(args, p.val)
+				}
+			}
+			sql += " " + context
+		} else {
+			if p.val != nil {
+				sql += " " + buildIdent(k) + " = ?"
+				args = append(args, p.val)
+			} else {
+				sql += " " + k
+			}
+		}
+	case nil:
+		sql += " ("
+		for j, c := range p.clause {
+			part, arg := c.Build(j)
+			sql += part
+			args = append(args, arg...)
+		}
+		sql += ")"
+	}
+	return sql, args
+}
+
+//Where ..
+func (s *SQLSegments) Where(key interface{}, vals ...interface{}) *SQLSegments {
+	s.where.Where(key, vals...)
+	return s
+}
+
+//OrWhere ..
+func (s *SQLSegments) OrWhere(key interface{}, vals ...interface{}) *SQLSegments {
+	s.where.OrWhere(key, vals...)
+	return s
+}
+
+//BuildWhereClause ...
+func (s *SQLSegments) buildWhereClause() string {
+	var sql string
+	if len(s.where.clause) > 0 {
+		sql = " WHERE"
+		for i, c := range s.where.clause {
+			part, args := c.Build(i)
+			sql += part
+			s.render.args = append(s.render.args, args...)
+		}
+	}
+	return sql
+}
+
+//Having ...
+func (s *SQLSegments) Having(key interface{}, vals ...interface{}) *SQLSegments {
+	s.having.Where(key, vals...)
+	return s
+}
+
+//buildHavingClause ...
+func (s *SQLSegments) buildHavingClause() string {
+	var sql string
+	if len(s.having.clause) > 0 {
+		sql = " WHERE"
+		for i, c := range s.having.clause {
+			part, args := c.Build(i)
+			sql += part
+			s.render.args = append(s.render.args, args...)
+		}
+	}
+	return sql
+}
+func (s *SQLSegments) buildFlags() string {
+	var sql string
+	for _, v := range s.flags {
+		sql += " " + v
+	}
+	return sql
+
+}
+func (s *SQLSegments) buildField() string {
+	var sql string
+	if len(s.fields) == 0 {
+		sql += " *"
+	} else {
+		for i, v := range s.fields {
+			if i > 0 {
+				sql += ","
+			}
+			if v == "*" {
+				sql += " " + v
+			} else {
+				sql += " " + buildIdent(v)
+			}
+
+		}
+	}
+	return sql
+}
+func (s *SQLSegments) buildTable() string {
+	var sql string
+	for i, v := range s.table {
+		if i > 0 {
+			sql += ","
+		}
+		sql += " " + buildIdent(v.Name)
+		if v.Alias != "" {
+			sql += " AS " + buildIdent(v.Alias)
+		}
+	}
+	return sql
+}
+func (s *SQLSegments) buildJoin() string {
+	var sql string
+	for _, t := range s.join {
+		sql += " " + t["type"] + "JOIN " + buildIdent(t["table"]) + " ON " + buildIdent(t["conditionA"]) + " " + t["logic"] + " " + buildIdent(t["conditionB"])
+	}
+	return sql
+}
+func (s *SQLSegments) buildGroupBy() string {
+	var sql string
+	if len(s.groupBy) > 0 {
+		sql += " GROUP BY"
+	}
+	for i, v := range s.groupBy {
+		if i > 0 {
+			sql += ","
+		}
+		sql += " " + buildIdent(v)
+	}
+	return sql
+}
+func (s *SQLSegments) buildOrderBy() string {
+	var sql string
+	if len(s.orderBy) > 0 {
+		sql += " ORDER BY"
+	}
+	for i, v := range s.orderBy {
+		if i > 0 {
+			sql += ","
+		}
+		sql += " " + buildIdent(v)
+	}
+	return sql
+}
+func (s *SQLSegments) buildLimit() string {
+	var sql string
+	if s.limit.offset != 0 {
+		sql += fmt.Sprintf(" OFFSET %d", s.limit.offset)
+	}
+	if s.limit.offset != 0 {
+		sql += fmt.Sprintf(" LIMIT %d", s.limit.limit)
+	}
+	return sql
+}
+
+//Union ...
+func (s *SQLSegments) Union(f func(*SQLSegments)) *SQLSegments {
+	s.union = append(s.union, f)
+	return s
+}
+func (s *SQLSegments) buildUnion() string {
+	var sql string
+	if len(s.union) > 0 {
+		sql += " UNION ("
+	}
+	for _, f := range s.union {
+		var ss = &SQLSegments{}
+		f(ss)
+		sql += ss.BuildSelect()
+	}
+	if len(s.union) > 0 {
+		sql += ")"
+	}
+	return sql
+}
+func (s *SQLSegments) buildForUpdate() string {
+	if s.forUpdate == true {
+		return " FOR UPDATE"
+	}
+	return ""
+}
+
+//BuildSelect ...
+func (s *SQLSegments) BuildSelect() string {
+	var sql = fmt.Sprintf("SELECT%s%s FORM%s%s%s%s%s%s%s%s%s",
+		s.buildFlags(),
+		s.buildField(),
+		s.buildTable(),
+		s.buildJoin(),
+		s.buildWhereClause(),
+		s.buildGroupBy(),
+		s.buildHavingClause(),
+		s.buildOrderBy(),
+		s.buildLimit(),
+		s.buildUnion(),
+		s.buildForUpdate(),
+	)
+	fmt.Println(s.render.args)
+	return sql
+}
+
+//Insert ...
+func (s *SQLSegments) Insert(params ...interface{}) *SQLSegments {
+	s.params = append(s.params, params...)
+	return s
+}
+
+//BuildInsert ...
+func (s *SQLSegments) BuildInsert() string {
+	var sql = fmt.Sprintf("INSERT%s INTO%s%s%s",
+		s.buildFlags(),
+		s.buildTable(),
+		s.buildValuesForInsert(),
+		s.buildReturning(),
+	)
+	return sql
+}
+
+//BuildReplace ...
+func (s *SQLSegments) BuildReplace() string {
+	var sql = fmt.Sprintf("REPLACE%s INTO%s%s%s",
+		s.buildFlags(),
+		s.buildTable(),
+		s.buildValuesForInsert(),
+		s.buildReturning(),
+	)
+	return sql
+}
+
+//BuildInsert ...
+func (s *SQLSegments) buildValuesForInsert() string {
+	var fields string
+	var values string
+	for i, param := range s.params {
+		v := reflect.ValueOf(param).Elem()
+		t := reflect.TypeOf(param).Elem()
+		if i == 0 {
+			values += " ("
+			fields += " ("
+		} else {
+			values += ",("
+		}
+		for j := 0; j < v.NumField(); j++ {
+			if v.Interface() == nil {
+				continue
+			}
+			var arg string
+			if t.Field(j).Tag.Get(tagKey) == "" {
+				arg = t.Field(j).Name
+			} else {
+				arg = t.Field(j).Tag.Get(tagKey)
+			}
+			s.render.args = append(s.render.args, v.Field(j).Interface())
+			// if v.Field(j).Kind() == reflect.String {
+			// 	// fmt.Printf(3"t:%v      v:%+v", arg, v.Field(j).Interface().(string))
+			// }
+			if j > 0 {
+				values += ","
+			}
+			values += "?"
+			if i == 0 {
+				if j > 0 {
+					fields += ","
+				}
+				fields += arg
+			}
+		}
+		if i == 0 {
+			fields += ")"
+		}
+		values += ")"
+	}
+	var sql = fields + " VALUES" + values
+	return sql
+}
+
+//Update ...
+func (s *SQLSegments) Update(params ...interface{}) *SQLSegments {
+	s.params = append(s.params, params...)
+	return s
+}
+
+//buildReturning ...
+func (s *SQLSegments) buildReturning() string {
+	if s.returning == true {
+		return " RETURNING"
+	}
+	return ""
+}
+
+//BuildUpdate ...
+func (s *SQLSegments) BuildUpdate() string {
+	var sql = fmt.Sprintf("UPDATE%s%s%s%s%s%s%s",
+		s.buildFlags(),
+		s.buildTable(),
+		s.buildValuesForUpdate(),
+		s.buildWhereClause(),
+		s.buildOrderBy(),
+		s.buildLimit(),
+		s.buildReturning(),
+	)
+	// fmt.Println(s.render.args)
+	return sql
+}
+
+//buildValuesForUpdate ...
+func (s *SQLSegments) buildValuesForUpdate() string {
+	var sql = " SET"
+	for i, param := range s.params {
+		v := reflect.ValueOf(param).Elem()
+		t := reflect.TypeOf(param).Elem()
+		if i == 0 {
+			for j := 0; j < v.NumField(); j++ {
+				if v.Interface() == nil {
+					continue
+				}
+				var arg string
+				if t.Field(j).Tag.Get(tagKey) == "" {
+					arg = t.Field(j).Name
+				} else {
+					arg = t.Field(j).Tag.Get(tagKey)
+				}
+				s.render.args = append(s.render.args, v.Field(j).Interface())
+				if j > 0 {
+					sql += ","
+				} else {
+					sql += " "
+				}
+				sql += arg + " = ?"
+			}
+		} else {
+			break
+		}
+	}
+	return sql
+}
+
+//Delete ...
+func (s *SQLSegments) Delete() *SQLSegments {
+	return s
+}
+
+//BuildDelete ...
+func (s *SQLSegments) BuildDelete() string {
+	var sql = fmt.Sprintf("DELETE%s FROM%s%s%s%s%s",
+		s.buildFlags(),
+		s.buildTable(),
+		s.buildWhereClause(),
+		s.buildOrderBy(),
+		s.buildLimit(),
+		s.buildReturning(),
+	)
+	// fmt.Println(s.render.args)
+	return sql
+}
+
+//buildIdent
+func buildIdent(name string) string {
+	return identKey + strings.Replace(name, ".", identKey+"."+identKey, -1) + identKey
+}
+
+//buildPlaceholder
+func buildPlaceholder(val interface{}) string {
+	var sql string
+	if reflect.TypeOf(val).Kind() == reflect.Slice {
+		for n := 0; n < reflect.ValueOf(val).Len(); n++ {
+			if n > 0 {
+				sql += ", ?"
+			} else {
+				sql += "?"
+			}
+		}
+	} else {
+		sql += "?"
+	}
+	return sql
+}
+
+//Connect ...
+type Connect struct {
+	db *sql.DB
+}
+
+//NewConnect ...
+func NewConnect() *Connect {
+	return &Connect{}
+}
+
+//TT ...
+type TT struct {
+	ID string `db:"id"`
+}
+
+type mapingStruct struct {
+	rt      reflect.Type
+	mapping map[string]*reflect.StructField
+}
+
+func (ms *mapingStruct) FieldByMaping(name string) *reflect.StructField {
+	if ptr, ok := ms.mapping[name]; ok {
+		return ptr
+	}
+	return nil
+}
+
+func newMapingStruct(src interface{}) mapingStruct {
+	rv := reflect.ValueOf(src)
+	t := rv.Type().Elem()
+	var ms mapingStruct
+	ms.rt = t
+	ms.mapping = make(map[string]*reflect.StructField, t.NumField())
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if !strings.Contains(string(field.Tag), tagKey+":") {
+			ms.mapping[field.Name] = &field
+			continue
+		}
+		tag := t.Field(i).Tag.Get(tagKey)
+		if tag == "-" {
+			continue
+		}
+		//todo db:"id,pk"
+		ms.mapping[tag] = &field
+	}
+	return ms
+}
+
+func (c *Connect) scanAll(rows *sql.Rows, dest interface{}) {
+	columns, _ := rows.Columns()
+	values := make([]interface{}, len(columns))
+	rv := reflect.ValueOf(dest)
+	fmt.Println("type:", rv.Kind())
+	if rv.Kind() == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	} else if rv.Kind() == reflect.Slice {
+		// slice := rv.Type()
+		// sliceTpye := rv.Type().Elem()
+		// ms := newMapingStruct(slice.Elem())
+		// fmt.Println(sliceTpye)
+		// fields := m.TraversalsByName(slice.Elem(), columns)
+		for rows.Next() {
+			// elem := reflect.New(sliceTpye).Elem()
+			// vp := reflect.New(slice.Elem())
+			// v := reflect.Indirect(vp)
+			// fmt.Println("===", v.Kind())
+			// fmt.Println(v.FieldByName("ID").Interface())
+			// values[0] = elem.FieldByName("ID").Interface()
+			// values[0] = elem.Field(0).Interface()
+			// if v.Kind() == reflect.Struct {
+			// for i := range columns {
+			// 	ptr := ms.FieldByMaping("id")
+			// 	if ptr != nil {
+			// 		values[i] = v.FieldByIndex(ptr.Index).Interface()
+			// 	} else {
+			// 		fmt.Println(v.Field(i).Interface())
+			// 		values[i] = v.Field(i).Interface()
+			// 	}
+			// }
+			// values[i] = v.Field(i).Addr().Interface()
+			rows.Scan(values...)
+		}
+		// for i := range columns {
+		//
+		// 	// values[i] = v.Field(i).Addr().Interface()
+		// 	// rt := reflect.TypeOf(vp.Interface())
+		// 	// values[i] = vp.Elem().Field(0).Addr().Interface()
+		// }
+	} else {
+
+	}
+	fmt.Println(dest)
+}
+
+//Connect ...
+func (c *Connect) Connect() {
+	var err error
+	c.db, err = sql.Open("mysql", "root:123321@tcp(192.168.33.10:3306)/auth")
+	// fmt.Println(err)
+	if err != nil {
+		log.Println(err)
+	}
+	defer c.db.Close()
+	err = c.db.Ping()
+	if err != nil {
+		log.Println(err)
+	}
+	rows, err := c.db.Query("select * from accounts")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// columns, _ := rows.Columns()
+	// values := make([]interface{}, len(columns))
+	// for rows.Next() {
+	// 	for i := range columns {
+	// 		values[i] = new(sql.RawBytes)
+	// 	}
+	// 	rows.Scan(values...)
+	// }
+
+	var dest []TT
+	c.scanAll(rows, dest)
+
+	// for i := range columns {
+	// 	values[i] = rv.Elem().Field(i).Addr().Interface()
+	// }
+	// fmt.Println("type:", rv.Kind())
+	// if rv.Kind() == reflect.Ptr {
+	// 	values[0] = rv.Elem().Field(0).Addr().Interface()
+	// } else if rv.Kind() == reflect.Slice {
+
+	// } else {
+	// 	rn := reflect.New(reflect.PtrTo(rv.Type()))
+
+	// 	// fmt.Println(rn.Elem().Type())
+	// 	// fmt.Println("----", rv.Kind() == reflect.Struct)
+	// 	// rn.Elem().Set(rv)
+
+	// 	// fmt.Println(rv.Addr())
+	// 	// rv.Set(rn.Elem())
+	// 	values[0] = rn.Elem().Addr().Interface()
+
+	// 	// values[0] = rv.Field(0).Addr().Interface()
+	// }
+	// fmt.Printf("%+v %+v\r\n", values, dest)
+	// fmt.Println("---", aa)
+	//在这里进行一些数据库操作
+}
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+
+	"./builder"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	s := builder.NewSQLSegment()
+	s.Flag("DISTANCE")
+	s.Field("*")
+	s.Table("tbl1.t1")
+	s.Where("t1.status", "0")
+	s.Where("type", "A")
+	s.Where("[in]sts", []string{"1", "2", "3", "4"})
+	s.Where("[in]sts2", 1)
+	s.Where(func(s *builder.Clause) {
+		s.Where("a", "200")
+		s.Where("b", "100")
+	})
+	s.Where("aaa = 999")
+	s.Where("[#]ccc = ?", 888)
+	s.Join("tbl3", "a", "=", "b")
+	s.Having("ss", "1")
+	s.Where("[~]a", "AA")
+	s.Where("[exists]", "AA")
+	s.Where("[exists]", func(s *builder.SQLSegments) {
+		s.Where("xx", 10000)
+	})
+	s.GroupBy("id")
+	s.OrderBy("id desc", "id asc")
+	s.Limit(30)
+	s.Offset(10)
+	s.ForUpdate()
+	fmt.Println(s.BuildSelect())
+
+	builder.NewConnect().Connect()
+
+	// s.Table("tbl1")
+	// s.Insert(&struct {
+	// 	Name string `db:"_name" db2:"test2"`
+	// 	Age  int    `db:"age"`
+	// }{"test", 16})
+	// s.Insert(&struct {
+	// 	Name string `db:"_name" db2:"test2"`
+	// 	Age  int    `db:"age"`
+	// }{"test", 16})
+	// fmt.Println(s.BuildInsert())
+
+	// s.Table("tbl1")
+	// s.Update(&struct {
+	// 	Name string `db:"_name" db2:"test2"`
+	// 	Age  int    `db:"age"`
+	// }{"test", 16})
+	// s.Update(&struct {
+	// 	Name string `db:"_name" db2:"test2"`
+	// 	Age  int    `db:"age"`
+	// }{"test", 16})
+	// s.Where("type", "A")
+	// fmt.Println(s.BuildUpdate())
+	//delate
+	// s.Table("tbl1.t1")
+	// s.Where("type", "A")
+	// s.Delete()
+	// fmt.Println(s.BuildDelete())
+
+	// c := &db.Clause{}
+	// c.Where("type", "A")
+	// c.Where("status", "0")
+	// c.Where(func(c *db.Clause) {
+	// 	c.Where("a", "200")
+	// 	c.Where("b", "100")
+	// 	c.Where(func(c *db.Clause) {
+	// 		c.Where("time", "2018")
+	// 		c.Where("you", 1)
+	// 	})
+	// })
+	// fmt.Println(c.Build(0))
+}
