15e83e47517eeab2fcd6ac02cfd0734af599a1b2
mlboy <mlboy@126.com>
2020-01-22 15:48:50 +0800

    WIP

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -459,9 +459,13 @@ func Plugins(dst interface{}, columns []string, targets []interface{}) error {
 	}
 	return nil
 }
+
+//Scan ..
 func Scan(rows *sql.Rows, dst interface{}) error {
 	return scanRow(rows, dst)
 }
+
+//ScanAll ..
 func ScanAll(rows *sql.Rows, dst interface{}) error {
 	defer rows.Close()
 	dstVal := reflect.ValueOf(dst)
@@ -503,20 +507,22 @@ func ScanAll(rows *sql.Rows, dst interface{}) error {
 		}
 	}
 }
-func isZeroVal(value reflect.Value) bool {
-	switch value.Kind() {
-	case reflect.String:
-		return value.Len() == 0
-	case reflect.Bool:
-		return !value.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return value.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return value.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return value.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return value.IsNil()
-	}
-	return reflect.DeepEqual(value.Interface(), reflect.Zero(value.Type()).Interface())
-}
+
+//使用内在方法
+// func isZeroVal(value reflect.Value) bool {
+// 	switch value.Kind() {
+// 	case reflect.String:
+// 		return value.Len() == 0
+// 	case reflect.Bool:
+// 		return !value.Bool()
+// 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+// 		return value.Int() == 0
+// 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+// 		return value.Uint() == 0
+// 	case reflect.Float32, reflect.Float64:
+// 		return value.Float() == 0
+// 	case reflect.Interface, reflect.Ptr:
+// 		return value.IsNil()
+// 	}
+// 	return reflect.DeepEqual(value.Interface(), reflect.Zero(value.Type()).Interface())
+// }
