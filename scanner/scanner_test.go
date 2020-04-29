2020da09f4109c54eea42b5d68812d4f546ae6bc
mlboy <mlboy@126.com>
2020-04-30 06:33:39 +0800

    test:增加测试用例

---
+++ scanner/scanner_test.go
@@ -0,0 +1,34 @@
+package scanner
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestResolveModelStruct(t *testing.T) {
+	type TestModel struct {
+		ID int `db:"id"`
+	}
+	m := TestModel{}
+	m.ID = 999999
+	ret, _ := ResolveModelStruct(m)
+	result := fmt.Sprintf("%+v", ret)
+	want := "&{table:TestModel columns:[id] fields:map[id:0xc00008e680] pk:id}"
+	if result != want {
+		t.Errorf("result: %v, want: %v", result, want)
+	}
+}
+
+func TestResolveStructValue(t *testing.T) {
+	type TestModel struct {
+		ID int `db:"id"`
+	}
+	m := TestModel{}
+	m.ID = 999999
+	ret, _ := ResolveStructValue(m)
+	result := fmt.Sprintf("%+v", ret)
+	want := "map[id:999999]"
+	if result != want {
+		t.Errorf("result: %v, want: %v", result, want)
+	}
+}
