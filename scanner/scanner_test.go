0705bd4dfe633e16c0f40904fff7a5ac6b9d3202
mlboy <mlboy@126.com>
2020-04-30 06:49:51 +0800

    test:调整测试用例

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -7,15 +7,19 @@ import (
 
 func TestResolveModelStruct(t *testing.T) {
 	type TestModel struct {
-		ID int `db:"id"`
+		ID int `db:"id,pk"`
 	}
 	m := TestModel{}
 	m.ID = 999999
 	ret, _ := ResolveModelStruct(m)
-	result := fmt.Sprintf("%+v", ret)
-	want := "&{table:TestModel columns:[id] fields:map[id:0xc00008e680] pk:id}"
-	if result != want {
-		// t.Errorf("result: %v, want: %v", result, want)
+	if ret.pk != "id" {
+		t.Errorf("result: pk=%v, want: pk=%v", ret.pk, "id")
+	}
+	if ret.GetStructField("id").isPrimaryKey != true {
+		t.Errorf("error: %v", `ret.GetStructField("id").isPrimaryKey`)
+	}
+	if ret.GetStructField("id").column != "id" {
+		t.Errorf("error: %v", `ret.GetStructField("id").column`)
 	}
 }
 
@@ -29,6 +33,6 @@ func TestResolveStructValue(t *testing.T) {
 	result := fmt.Sprintf("%+v", ret)
 	want := "map[id:999999]"
 	if result != want {
-		// t.Errorf("result: %v, want: %v", result, want)
+		t.Errorf("result: %v, want: %v", result, want)
 	}
 }
