1e560e6abe9171574ff295755578c154a90373ca
mlboy <mlboy@126.com>
2020-05-19 08:21:42 +0800

    fix:删掉多余的测试覆盖

--- pool_test.go
+++ pool_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestDbEngine(t *testing.T) {
@@ -21,7 +22,7 @@ func TestDbEngine(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
-func TestNewCluster(t *testing.T) {
+func TestNewCluster1(t *testing.T) {
 	Debug = true
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -31,7 +32,23 @@ func TestNewCluster(t *testing.T) {
 
 	c := NewCluster()
 	c.Exec("select 1")
-	t.Error(c)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+func TestNewCluster2(t *testing.T) {
+	Debug = true
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	c := NewCluster(
+		AddDb("mysql", "", SetConnMaxLifetime(1), SetMaxIdleConns(1), SetMaxOpenConns(1)),
+	)
+	c.Exec("select 1")
 
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
